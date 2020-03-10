16a467aa3dabf8697d319afd4dcb71c93e257531
fatihcinar1 <42092052+fatihcinar1@users.noreply.github.com>
2020-03-11 02:08:58 +0300

    Create Reverser.java

---
+++ Solutions/Reverse a String Using Stack/Java/Reverser.java
@@ -0,0 +1,26 @@
+
+class Reverser
+{
+    public static void reverseString(StringBuffer string){
+
+        final char TERMINATION_CHARACTER = '\0';
+
+        Stack stack = new Stack();
+
+        final int lengthString = string.length();
+
+        for(int iterator = 0; iterator < lengthString; iterator++){
+
+            stack.push(string.charAt(iterator));
+
+        }
+
+        for(int iterator = 0; !stack.isEmpty(); iterator++){
+
+            string.setCharAt(iterator, stack.pop());
+        }
+
+        return;
+
+    }
+}
