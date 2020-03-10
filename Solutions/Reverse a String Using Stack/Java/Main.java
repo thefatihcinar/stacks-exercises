70fb6242c5dd5c839a67c95902dd1b6473a5a738
fatihcinar1 <42092052+fatihcinar1@users.noreply.github.com>
2020-03-11 02:08:44 +0300

    Create Main.java

---
+++ Solutions/Reverse a String Using Stack/Java/Main.java
@@ -0,0 +1,11 @@
+
+class Main
+{
+    public static void main(String[] args){
+
+        StringBuffer str = new StringBuffer("fatih");
+        Reverser.reverseString(str);
+
+        System.out.println(str);
+    }
+}
